86114c2e5ec6fad6dc47ddce7fee4c69a73f17cb
zakpaw <zakpaw36@gmail.com>
2020-12-07 15:01:50 +0100

    rename

--- NN.py
+++ NN.py
@@ -16,6 +16,11 @@ class Neural_Network:
     def sigmoid(x):
         return 1/(1+np.exp(-x))
 
+    @staticmethod
+    def sigmoid_prime(x):
+        exp = np.exp(-x)
+        return exp/((1+exp)**2)
+
     def forward(self, x):
         self.z2 = np.dot(x, self.W1)
         self.a2 = self.sigmoid(self.z2)
@@ -23,8 +28,24 @@ class Neural_Network:
         yh = self.sigmoid(self.z3)
         return yh
 
+    def cost_fun(self, X, y):
+        self.yh = self.forward(X)
+        return 0.5*sum((y-self.yh)**2)
+
+    def cost_fun_prime(self, X, y):
+        self.yh = self.forward(X)
+
+        delta3 = np.multiply(-(y-self.yh), self.sigmoid_prime(self.z3))
+        derivJW2 = np.dot(self.a2.T, delta3)
+
+        delta2 = np.dot(delta3, self.W2.T) * self.sigmoid_prime(self.z2)
+        derivJW1 = np.dot(X.T, delta2)
+
+        return derivJW1, derivJW2
+
 
 if __name__ == '__main__':
     X, y = spiral_data(100, 3)
     nn = Neural_Network(input_l=2, output_l=3, hidden_l=2)
-    print(nn.forward(X))
+    print(y)
+    print(nn.cost_fun_prime(X, y))
